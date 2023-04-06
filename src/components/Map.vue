90606cd9e3b8a7ad53ff8e15ccf33d408c398363
Adebola Adeola <debola_adeola@yahoo.com>
2023-04-06 13:19:51 +0100

    Update Map.vue

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,12 +1,7 @@
 <template>
   <div class="">
-<<<<<<< HEAD
     <img :src="'https://alkadelik.pythonanywhere.com' + map.map" alt="segment map">
     <h4>{{ map.segment }}</h4>
-=======
-    <img :src="'https://alkadelik.pythonanywhere.com' + map" alt="" width="200px">
->>>>>>> f821de361ec4a8d942a546aafd8da7a69016d31b
-
   </div>
 </template>
 
