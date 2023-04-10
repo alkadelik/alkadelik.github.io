f597afc9315afd0c1be097079363457ad3412811
Adebola Adeola <alkadelik@Adebolas-MacBook-Pro.local>
2023-04-10 16:22:23 +0100

    small fix

--- src/services/urls.js
+++ src/services/urls.js
@@ -1,4 +1,4 @@
-export const baseUrl = 'https://alkadelik.pythonanywhere.com'
+export const baseUrl = 'https://alkadelik.pythonanywhere.com/'
 
 // internal
 export const fetchRoads = `${baseUrl}roads/api/list/`
