e9d356149536451717cadcc91b7aeae60aebe0e2
Adebola Adeola <alkadelik@Adebolas-MacBook-Pro.local>
2023-04-25 12:50:46 +0100

    updated url

--- src/services/urls.js
+++ src/services/urls.js
@@ -1,4 +1,4 @@
-export const baseUrl = 'https://www.pythonanywhere.com/'
+export const baseUrl = 'https://www.alkadelik.pythonanywhere.com/'
 
 // internal
 export const fetchSegments = `${baseUrl}roads/api/list/`
